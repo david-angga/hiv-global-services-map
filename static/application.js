f0331d5ff41df67850d0a864362f9a82884b0832
David <david@david-desktop.(none)>
2012-08-29 21:46:59 +0700

    Refactor the javascript map functions, move reusable codes to /static/application.js

--- static/application.js
+++ static/application.js
@@ -52,3 +52,60 @@ function httpGet(theUrl){
   return http.responseText;
 }
 
+// Return facility description for infoWindow
+function getFacilityInformation(serviceDesc, completeAddress, locationName, locationPhoto, isPhotoExist){
+  var imageUrl;
+  var completeDescription = '<b>'+locationName+'</b><br>'+ serviceDesc;
+
+  // Check if location has a photo
+  if (isPhotoExist == 1){
+    completeDescription += '<br><img src="/img?img_id='+ locationPhoto +'" width=50% height=50% id="'+ locationPhoto +'">';
+  }
+  
+  completeDescription += '<br><small>'+ completeAddress +'</small>';
+  
+  return completeDescription;
+}
+
+// Show a map centered at (position.coords.latitude, position.coords.longitude).
+function showMap(position, zoomValue, mapElement){
+  var map;
+  var mapOptions = {
+    center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+    zoom: zoomValue,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById(mapElement), mapOptions);
+  
+  return map;
+}
+
+// Marked on locations along with the info window
+function showMarker(map, latitude, longitude, titleName, infoContent){
+  var latLng = new google.maps.LatLng(latitude, longitude);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: titleName
+  });
+  
+  var infoWindow = new google.maps.InfoWindow();
+  
+  google.maps.event.addListener(marker, 'click', (function setInfoWindow(marker){
+    return function(){
+      infoWindow.setPosition(latLng);
+      infoWindow.setContent(infoContent);
+      infoWindow.open(map, marker); 
+    }    
+  })(marker));
+  
+  return marker;
+}
+
+// Show location error
+function displayError(positionError) {
+  alert("Could not determine location.");
+}
+
+
